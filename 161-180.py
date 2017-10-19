4d8e939d63514ff061146a58f58619a5a7b6ada9
xianzzong <xzzong@gmail.com>
2017-10-19 05:17:37 -0700

    161-180

--- 161-180.py
+++ 161-180.py
@@ -53,9 +53,156 @@ def maximumGap(nums):
             maxgap=max(maxgap,buckets[y]['min']-buckets[x]['max'])
         x=y
     return maxgap
-import random
-#test=[random.randint(0,100) for i in range(10)]
-test1=[26, 69, 79, 74, 67, 5, 33, 41, 13, 46]
-result=maximumGap(test1)
-res=sorted(test1)
-print result
+# import random
+# #test=[random.randint(0,100) for i in range(10)]
+# test1=[26, 69, 79, 74, 67, 5, 33, 41, 13, 46]
+# result=maximumGap(test1)
+# res=sorted(test1)
+# print result
+def compareVersion(version1, version2):
+    """
+    :type version1: str
+    :type version2: str
+    :rtype: int
+    """
+    v1=version1.split('.')
+    v2=version2.split('.')
+    len_1=len(v1);len_2=len(v2)
+    length=min(len_1,len_2)
+    i=0
+    while i<length:
+        tmp1=int(v1[i])
+        tmp2=int(v2[i])
+        if tmp1==tmp2:
+            i+=1
+        elif tmp1<tmp2:
+            return -1
+        else:
+            return 1
+    if i==len_1 and i!=len_2:
+        while i<len_2:
+            if int(v2[i])>0:
+                return -1
+            i+=1
+    elif i!=len_1 and i==len_2:
+        while i<len_1:
+            if int(v1[i])>0:
+                return 1
+            i+=1
+    return 0
+# test=['1.0','1']
+# res=compareVersion(test[0],test[1])
+# print res
+def fractionToDecimal(numerator, denominator):
+    if (numerator>=0 and denominator>0) or (numerator<=0 and denominator<0):
+        result=''
+    else:
+        result='-'
+    numerator=abs(numerator);denominator=abs(denominator)
+    tmp_quotient=numerator/denominator
+    tmp_remain=numerator%denominator
+    result=result+str(tmp_quotient)
+    quotient=[]
+    remain=[]
+    remain_index=-1
+    while tmp_remain:
+        remain.append(str(tmp_remain))
+        tmp=tmp_remain*10
+        tmp_quotient=tmp/denominator
+        tmp_remain=tmp%denominator
+        quotient.append(str(tmp_quotient))
+        if (str(tmp_remain) in remain):
+            remain_index=remain.index(str(tmp_remain))
+            break
+    if len(quotient) and remain_index!=-1:
+        result=result+'.'+''.join(quotient[0:remain_index])+'('+''.join(quotient[remain_index:])+')'
+    elif len(quotient) and remain_index==-1:
+        result=result+'.'+''.join(quotient)
+    return result
+# res=fractionToDecimal(20,5)
+# print res
+def twoSum(numbers, target):
+    len_n=len(numbers)
+    left=0;right=len_n-1
+    while left<right:
+        if numbers[left]+numbers[right]>target:
+            right-=1
+        elif numbers[left]+numbers[right]<target:
+            left+=1
+        else:
+            break
+    if left==right:
+        return 0
+    return [left+1,right+1]
+# test=[2,3,4]
+# result=twoSum(test,6)
+# print result
+def convertToTitle(n):
+    res=''
+    while n:
+        res=chr(ord('A')+(n-1)%26)+res
+        n=(n-1)/26
+    return res
+# res=convertToTitle(298)
+# print res
+def majorityElement(nums):
+    len_n=len(nums)
+    result=nums[0]
+    res_num=1
+    for i in range(1,len_n):
+        if res_num==0:
+            result=nums[i]
+            res_num=1
+        else:
+            if nums[i]==result:
+                res_num+=1
+            else:
+                res_num-=1
+    return result
+# test=[1,2,1,2,3,1,1]
+# res=majorityElement(test)
+# print res
+def titleToNumber(s):
+    res=0
+    for item in s:
+        res=res*26+ord(item)-ord('A')+1
+    return res
+# res=titleToNumber(convertToTitle(256))
+# print res
+def trailingZeroes(n):
+    res=0
+    while n>=5:
+        res+=n/5
+        n=n/5
+    return res
+# res=trailingZeroes(30)
+# print res
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class BSTIterator(object):
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.result=[]
+        self.putleft(root)
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        self.result
+    def next(self):
+        """
+        :rtype: int
+        """
+        res=self.result.pop()
+        self.putleft(res.right)
+        return res.val
+    def putleft(self,node):
+        while node:
+            self.result.append(node)
+            node=node.left
